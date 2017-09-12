e19fd8fc808b4a1523e00acdae29b01db6ca145d
Larionov Nikita <iksiygrik@gmail.com>
2017-09-13 01:21:02 +0300

    SimpleThreadPool

---
+++ SimpleThreadPool/main.cpp
@@ -0,0 +1,29 @@
+#include "threadPool.h"
+
+void thread1() {
+	cout << "thread 1\n";
+	cout << "still thread 1\n";
+	cout << "still thread 1\n";
+	cout << "still thread 1\n";
+	cout << "still thread 1\n";
+	cout << "still thread 1\n";
+	this_thread::sleep_for(chrono::milliseconds(100));
+	cout << "still thread 1\n";
+	cout << "still thread 1\n";
+	cout << "still thread 1\n";
+	cout << "still thread 1\n";
+}
+
+void thread2() {
+	cout << "thread 2\n";
+}
+
+int main() {
+	ThreadPool pool(10);
+
+	pool.runAsync(thread1);
+	pool.runAsync(thread2);
+	
+	this_thread::sleep_for(chrono::milliseconds(1));
+	return 0;
+}
