d922866a1ab6ba000786d189d76c9ee551a17b42
Larionov Nikita <iksiygrik@gmail.com>
2017-09-13 01:07:22 +0300

    Revert "Complexity separation"
    This reverts commit a7cf3cebae5d9a6aae6c4a644e7d9361f23c0a79.

---
+++ poolThread.h
@@ -0,0 +1,32 @@
+#ifndef POOLTHREAD
+#define POOLTHREAD
+
+#include <thread>
+#include <vector>
+#include <list>
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <iostream>
+using namespace std;
+
+class ThreadPool {
+private:
+	void ThreadLoop();
+	function<void()> Wait_for_next_func();
+
+	vector<unique_ptr<thread> > threads;
+	list<function<void()> > functions;
+
+	mutex function_mutex;
+	condition_variable funcList;
+	atomic<bool> isTerminated;
+public:
+	ThreadPool(size_t numThreads);
+	virtual ~ThreadPool();
+
+	void execute(function<void()> const&);
+};
+
+#endif
